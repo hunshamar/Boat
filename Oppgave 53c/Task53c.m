e62ead9f50c1161e7471ab5234b656e1afd752f4
Asgeir <asgeirhuns@gmail.com>
2018-10-30 21:09:16 +0100

    Gjort oppgave PD; ikke ført inn alt

---
+++ Oppgave 53c/Task53c.m
@@ -0,0 +1,25 @@
+
+
+%Constants
+
+K_pd = 0.8362;
+T_d = 72.5216;
+T_f = 8.39;
+psi_r = 30;
+
+compass = importdata('53c_compass.mat'); %Import data from simulink
+rudder = importdata('53c_rudder.mat'); %Import data from simulink
+reference = importdata('53c_ref.mat'); %Import data from simulink
+
+n=1:length(compass(2,:));
+
+plot(n,compass(2,n), 'color', 'r'); %Plot compass
+hold on;
+plot(n,rudder(2,n), 'color', 'b'); %Plot rudder
+hold on;
+plot(n,reference(2,n), 'green--'); %Plot reference
+
+xlabel('time'); ylabel('angle');
+legend('compass', 'rudder', 'reference');
+axis([0 500 -20 40]);
+
