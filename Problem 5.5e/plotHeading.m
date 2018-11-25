4207cb5ac8ac0ad7c79b94d65d4897b70c229538
KjetilSekseKristiansen <36543031+KjetilSekseKristiansen@users.noreply.github.com>
2018-11-25 19:13:38 +0100

    Add files via upload

---
+++ Problem 5.5e/plotHeading.m
@@ -0,0 +1,14 @@
+%% code for plotting estimated and actual heading together %% 
+
+no = importdata('53e_compass.mat'); %Import data from simulink
+ff = importdata('53e_estimatedHeading.mat');
+plot(1:0.1:length(no)/10,no(2,1:length(1:0.1:length(no)/10)), 'r');
+hold on;
+plot(1:0.1:length(ff)/10,ff(2,1:length(1:0.1:length(ff)/10)), 'b');
+grid on;
+title('Actual and estimated wave heading');
+ axis([0 500 -5 5]);
+xlabel('time');
+ylabel('travel angle');
+legend('Actual compass course', 'estimated heading');
+hold off;
